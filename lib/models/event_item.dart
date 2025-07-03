f7d3421250916aa1fcbae7873ee97d56cfab3b9e
Seima <seimatanaka123@gmail.com>
2025-07-04 01:21:47 +0900

    event_item.dartの改善・バックエンドができるまで仮で使用するダミーの企画データリスト（dummy_events.dart）を作成

--- lib/models/event_item.dart
+++ lib/models/event_item.dart
@@ -1,27 +1,59 @@
 ﻿// event_item.dart
 
+// 開催日を表現するためのenum
+enum FestivalDay {
+  dayOne, // 1日目
+  dayTwo, // 2日目
+  both,   // 両日
+}
+
+// 開催エリアを表現するためのenum
+enum EventArea {
+  building1,
+  building2,
+  building3,
+  building4,
+  building5, // 体育館
+  outdoor, // 屋外模擬店
+  other,        // その他
+}
+
+// 企画カテゴリを表現するためのenum
+enum EventCategory {
+  stage,      // ステージ
+  exhibit,    // 展示
+  food,       // 飲食
+  handsOn,    // 体験型
+  game,       //ゲーム
+  other,      // その他
+}
+
+
 // 一つの企画が持つ情報を定義するクラス
 class EventItem {
-  final String id; // 各企画を区別するID
-  final String title; // 企画名
-  final String groupName; // 団体名
-  final String description; // 企画の詳しい説明文
-  final String imagePath; // サムネイル画像のパス
-  final String location; // 開催場所
-  final String category; // カテゴリ
-  final DateTime? startTime; // 開始時間 (時間指定のない企画はnull)
-  final DateTime? endTime;   // 終了時間 (時間指定のない企画はnull)
+  final String id;
+  final String title;
+  final String groupName;
+  final String description;
+  final String imagePath;
+  final EventArea area;
+  final String location;
+  final EventCategory category;
+  final FestivalDay date;
+  final DateTime? startTime;
+  final DateTime? endTime;
 
-  // このクラスからインスタンスを作るコンストラクタ
   const EventItem({
     required this.id,
     required this.title,
     required this.groupName,
     required this.description,
     required this.imagePath,
+    required this.area,
     required this.location,
     required this.category,
-    this.startTime, // 決まった時刻がないものもあるのでrequiredではない
+    required this.date,
+    this.startTime,
     this.endTime,
   });
 }
