343772b071bcc9c1bf16f37dd7565fcd6c1f5140
Seima <seimatanaka1123@gmail.com>
2025-07-08 17:45:37 +0900

    お知らせ一覧画面の内容を書くファイルを追加

---
+++ lib/screens/announcement_screen.dart
@@ -0,0 +1,13 @@
+﻿import 'package:flutter/material.dart';
+
+class AnnouncementScreen extends StatelessWidget {
+  const AnnouncementScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('お知らせ一覧')),
+      body: const Center(child: Text('ここにお知らせを掲載します')),
+    );
+  }
+}
