d6b3a758cdfa32577c0b82c797a58a50c241f248
Seima <seimatanaka1123@gmail.com>
2025-08-01 17:21:05 +0900

    注目企画のデータモデルを作成

---
+++ lib/models/spotlight_item.dart
@@ -0,0 +1,20 @@
+﻿enum SpotlightActionType {
+  event,
+  url,
+}
+
+class SpotlightItem {
+  final String id;
+  final String imagePath;
+  final bool isVisible;
+  final SpotlightActionType actionType;
+  final String actionValue;
+
+  const SpotlightItem({
+    required this.id,
+    required this.imagePath,
+    this.isVisible = true,
+    required this.actionType,
+    required this.actionValue,
+  });
+}
